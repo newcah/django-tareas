320e70c5afe4933ef91b54e2c9d8bf71b135fe36
newcah <newcah2028@gmail.com>
2023-06-06 19:35:31 -0300

    settings modify

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:postgres',        conn_max_age=600)}
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600)}
 
 
 # Password validation
